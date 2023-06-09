3ad9b9a9b029ada4d5c973b148816930674e77d4
AussieKing <126050763+AussieKing@users.noreply.github.com>
2023-06-09 12:09:20 +1000

    CRUD operations in server.js done, time to test

--- server.js
+++ server.js
@@ -51,28 +51,28 @@ function start() {
             case 'Update Employee Role': //! CREATED
                 updateEmployeeRole();
                 break;
-            case 'View All Roles':
+            case 'View All Roles': //! CREATED
                 viewAllRoles();
                 break;
-            case 'Add Role':
+            case 'Add Role': //! CREATED
                 addRole();
                 break;
-            case 'View All Departments':
+            case 'View All Departments': //! CREATED
                 viewAllDepartments();
                 break;
-            case 'Add Department':
+            case 'Add Department': //! CREATED
                 addDepartment();
                 break;
-            case 'Update Employee Manager':
+            case 'Update Employee Manager': //! CREATED
                 updateEmployeeManager();
                 break;
-            case 'View Employee By Manager':
+            case 'View Employee By Manager': //! CREATED
                 viewEmployeeByManager();
                 break;
-            case 'View Employee By Department':
+            case 'View Employee By Department': //! CREATED
                 viewEmployeeByDepartment();
                 break;
-            case 'Delete Employee / Role / Department':
+            case 'Delete Employee / Role / Department': //! CREATED
                 deleteEmployeeRoleDepartment();
                 break;
             case 'Quit':
@@ -84,6 +84,8 @@ function start() {
         }
     }
 )};
+
+//! ******* Start of all the CRUD functions ********
 
 //! function to view all Employees
 function viewAllEmployees() {
@@ -211,5 +213,227 @@ function updateEmployeeRole() {
     });
 }
 
+//! function to view all roles
+function viewAllRoles() {
+    const query =
+    `SELECT role.id AS Id,
+    role.title AS Title, 
+    department.name AS Department, 
+    role.salary AS Salary,
+    FROM role
+    LEFT JOIN department ON role.department_id = department.id
+    ORDER BY role.id ASC;`;
 
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+}
 
+//! function to add a role
+function addRole() {
+    db.query('SELECT id, name FROM department', (err, res) => {
+        if (err) throw err;
+
+        const departments = res.map(({ id, name }) => ({
+            name: name,
+            value: id
+        }));
+
+        inquirer.prompt([
+            {
+                name: 'title',
+                type: 'input',
+                message: "What is the role's title?"
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: "What is the role's salary?"
+            },
+            {
+                name: 'department_id',
+                type: 'list',
+                message: "What is the role's department?",
+                choices: departments
+            }
+        ])
+        .then(answer => {
+            db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', 
+            [answer.title, answer.salary, answer.department_id], 
+            (err, res) => {
+                if (err) throw err;
+                console.log('Role added to database successfully.');
+                start();
+            });
+        });
+    });
+}
+
+//! function to view all departments
+function viewAllDepartments() {
+    const query =
+    `SELECT department.id AS Id,
+    department.name AS Department
+    FROM department
+    ORDER BY department.id ASC;`;
+
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+}
+
+//! function to add a department
+function addDepartment() {
+    inquirer.prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: "What is the department's name?"
+        }
+    ])
+    .then(answer => {
+        db.query('INSERT INTO department (name) VALUES (?)', 
+        [answer.name], 
+        (err, res) => {
+            if (err) throw err;
+            console.log('Department added to database successfully.');
+            start();
+        });
+    });
+}
+
+//! function to view all employees by department
+function viewAllEmployeesByDepartment() {
+    const query =
+    `SELECT employee.id AS Id,
+    employee.first_name AS 'First Name',
+    employee.last_name AS 'Last Name',
+    department.name AS Department
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    ORDER BY employee.id ASC;`;
+
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+}
+
+//! function to view all employees by manager
+function viewAllEmployeesByManager() {
+    const query =
+    `SELECT employee.id AS Id,
+    employee.first_name AS 'First Name',
+    employee.last_name AS 'Last Name',
+    CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+    FROM employee
+    LEFT JOIN employee manager ON manager.id = employee.manager_id
+    ORDER BY employee.id ASC;`;
+
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+}
+
+//! function to delete an employee
+function deleteEmployee() {
+    db.query('SELECT id, first_name, last_name FROM employee', (err, res) => {
+        if (err) throw err;
+
+        const employees = res.map(({ id, first_name, last_name }) => ({
+            name: `${first_name} ${last_name}`,
+            value: id
+        }));
+
+        inquirer.prompt([
+            {
+                name: 'employee_id',
+                type: 'list',
+                message: "Which employee would you like to delete?",
+                choices: employees
+            }
+        ])
+        .then(answer => {
+            db.query('DELETE FROM employee WHERE id = ?', 
+            [answer.employee_id], 
+            (err, res) => {
+                if (err) throw err;
+                console.log('Employee deleted from database successfully.');
+                start();
+            });
+        });
+    });
+}
+
+//! function to delete a role
+function deleteRole() {
+    db.query('SELECT id, title FROM role', (err, res) => {
+        if (err) throw err;
+
+        const roles = res.map(({ id, title }) => ({
+            name: title,
+            value: id
+        }));
+
+        inquirer.prompt([
+            {
+                name: 'role_id',
+                type: 'list',
+                message: "Which role would you like to delete?",
+                choices: roles
+            }
+        ])
+        .then(answer => {
+            db.query('DELETE FROM role WHERE id = ?', 
+            [answer.role_id], 
+            (err, res) => {
+                if (err) throw err;
+                console.log('Role deleted from database successfully.');
+                start();
+            });
+        });
+    });
+}
+
+//! function to delete a department
+function deleteDepartment() {
+    db.query('SELECT id, name FROM department', (err, res) => {
+        if (err) throw err;
+
+        const departments = res.map(({ id, name }) => ({
+            name: name,
+            value: id
+        }));
+
+        inquirer.prompt([
+            {
+                name: 'department_id',
+                type: 'list',
+                message: "Which department would you like to delete?",
+                choices: departments
+            }
+        ])
+        .then(answer => {
+            db.query('DELETE FROM department WHERE id = ?', 
+            [answer.department_id], 
+            (err, res) => {
+                if (err) throw err;
+                console.log('Department deleted from database successfully.');
+                start();
+            });
+        });
+    });
+}
+
+//! close connection to database
+process.on('exit', () => {
+    db.end();
+});
